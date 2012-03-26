4e8083e2f5da3ea91686298c4ca12c95e365e92a
yoflippo@gmail.com <yoflippo@gmail.com@c7ecc483-4039-c1f6-3668-045aa2de619e>
2012-03-26 09:26:38 +0000

    some adjustments, better graph.

--- trunk/ btlabjackstreamer/BT Labjack Stream/BT Labjack Stream/frmGraph.Designer.cs
+++ trunk/ btlabjackstreamer/BT Labjack Stream/BT Labjack Stream/frmGraph.Designer.cs
@@ -31,13 +31,15 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGraph));
             this.zg1 = new ZedGraph.ZedGraphControl();
-            this.nudGraphX = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.nudGraphY = new System.Windows.Forms.NumericUpDown();
+            this.gbx_Settings = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.nudGraphX)).BeginInit();
+            this.nudGraphY = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudGraphX = new System.Windows.Forms.NumericUpDown();
+            this.gbx_Settings.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudGraphY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGraphX)).BeginInit();
             this.SuspendLayout();
             // 
             // zg1
@@ -57,18 +59,49 @@
             this.zg1.TabIndex = 13;
             this.zg1.ZoomStepFraction = 1D;
             // 
-            // nudGraphX
+            // timer1
             // 
-            this.nudGraphX.Location = new System.Drawing.Point(12, 482);
-            this.nudGraphX.Maximum = new decimal(new int[] {
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // gbx_Settings
+            // 
+            this.gbx_Settings.Controls.Add(this.label1);
+            this.gbx_Settings.Controls.Add(this.nudGraphY);
+            this.gbx_Settings.Controls.Add(this.label2);
+            this.gbx_Settings.Controls.Add(this.nudGraphX);
+            this.gbx_Settings.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbx_Settings.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gbx_Settings.Location = new System.Drawing.Point(12, 498);
+            this.gbx_Settings.Name = "gbx_Settings";
+            this.gbx_Settings.Size = new System.Drawing.Size(298, 69);
+            this.gbx_Settings.TabIndex = 20;
+            this.gbx_Settings.TabStop = false;
+            this.gbx_Settings.Text = "Settings";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(144, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Y-as";
+            // 
+            // nudGraphY
+            // 
+            this.nudGraphY.Location = new System.Drawing.Point(147, 36);
+            this.nudGraphY.Maximum = new decimal(new int[] {
             1000000,
             0,
             0,
             0});
-            this.nudGraphX.Name = "nudGraphX";
-            this.nudGraphX.Size = new System.Drawing.Size(120, 20);
-            this.nudGraphX.TabIndex = 15;
-            this.nudGraphX.Value = new decimal(new int[] {
+            this.nudGraphY.Name = "nudGraphY";
+            this.nudGraphY.Size = new System.Drawing.Size(120, 22);
+            this.nudGraphY.TabIndex = 22;
+            this.nudGraphY.Value = new decimal(new int[] {
             10,
             0,
             0,
@@ -79,75 +112,59 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(12, 466);
+            this.label2.Location = new System.Drawing.Point(6, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 16);
-            this.label2.TabIndex = 17;
+            this.label2.TabIndex = 21;
             this.label2.Text = "X-as";
             // 
-            // timer1
+            // nudGraphX
             // 
-            this.timer1.Enabled = true;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // nudGraphY
-            // 
-            this.nudGraphY.Location = new System.Drawing.Point(153, 482);
-            this.nudGraphY.Maximum = new decimal(new int[] {
+            this.nudGraphX.Location = new System.Drawing.Point(6, 36);
+            this.nudGraphX.Maximum = new decimal(new int[] {
             1000000,
             0,
             0,
             0});
-            this.nudGraphY.Name = "nudGraphY";
-            this.nudGraphY.Size = new System.Drawing.Size(120, 20);
-            this.nudGraphY.TabIndex = 18;
-            this.nudGraphY.Value = new decimal(new int[] {
+            this.nudGraphX.Name = "nudGraphX";
+            this.nudGraphX.Size = new System.Drawing.Size(120, 22);
+            this.nudGraphX.TabIndex = 20;
+            this.nudGraphX.Value = new decimal(new int[] {
             10,
             0,
             0,
             0});
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(150, 466);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 16);
-            this.label1.TabIndex = 19;
-            this.label1.Text = "Y-as";
             // 
             // frmGraph
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkRed;
-            this.ClientSize = new System.Drawing.Size(905, 517);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.nudGraphY);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.nudGraphX);
+            this.ClientSize = new System.Drawing.Size(905, 579);
+            this.Controls.Add(this.gbx_Settings);
             this.Controls.Add(this.zg1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmGraph";
             this.Text = "Grafiek";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmGraph_FormClosing);
-            ((System.ComponentModel.ISupportInitialize)(this.nudGraphX)).EndInit();
+            this.Resize += new System.EventHandler(this.frmGraph_Resize);
+            this.gbx_Settings.ResumeLayout(false);
+            this.gbx_Settings.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudGraphY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGraphX)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private ZedGraph.ZedGraphControl zg1;
-        private System.Windows.Forms.NumericUpDown nudGraphX;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.NumericUpDown nudGraphY;
+        private System.Windows.Forms.GroupBox gbx_Settings;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudGraphY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudGraphX;
     }
 }
